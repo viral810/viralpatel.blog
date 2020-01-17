e70bb0fbff04f0fefc3a909b9811a94f1c5c5782
Viral Patel <pviral810@gmail.com>
2020-01-17 16:37:44 -0500

    Add global search component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -60,6 +60,7 @@ export default class Navbar extends Component {
           >
             <img src={kofi} alt='Kofi' className='kofi' />
           </a>
+          <input type="text" class="st-default-search-input bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"></input>
         </div>
       </nav>
     );
--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -36,6 +36,12 @@ class MainLayout extends React.Component {
           <meta name='description' content={config.siteDescription} />
           <html lang='en' />
           <link rel='shortcut icon' type='image/png' href={favicon} />
+          <script type='text/javascript'>{`(function(w,d,t,u,n,s,e){w['SwiftypeObject']=n;w[n]=w[n]||function(){
+  (w[n].q=w[n].q||[]).push(arguments);};s=d.createElement(t);
+  e=d.getElementsByTagName(t)[0];s.async=1;s.src=u;e.parentNode.insertBefore(s,e);
+  })(window,document,'script','//s.swiftypecdn.com/install/v2/st.js','_st');
+
+  _st('install','5hZaKcJz5iWg-oKoJWqP','2.0.0');`}</script>
         </Helmet>
 
         {/* Page Body should go here */}
